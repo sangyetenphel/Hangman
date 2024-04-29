1b986357740a000535bc78ca5f2568b5f7acba79
sangyetenphel <sangyetenphel@gmail.com>
2024-04-29 08:32:53 -0400

    Added styling to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import { useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import '../styles/Home.css'
+import ropeImage from '../images/rope.png';
 
 function Home() {
     const navigate = useNavigate();
@@ -13,19 +14,20 @@ function Home() {
 
     return (
         <div className='container'>
+            <img src={ropeImage} alt="" className='ropeImage'/>
             <h1 className='title'>Hangman</h1>
             <div className="offline-online-container">
-                <div>
-                    <button className='button-red' onClick={()=> handleClick("offline")}>Play offline</button>
+                <div className='dialog-info-box'>
                     <p>Experience the classic hangman game offline, where you'll be challenged with a series of randomly generated words. Put your word-solving skills to the test and see if you have what it takes to emerge victorious!</p>
+                    <button className='button button-offline' onClick={()=> handleClick("offline")}>Play offline</button>
                 </div>
-                <div>
-                <button className='button-green' onClick={()=> handleClick("online")}>Play online</button>
-                <p>Challenge your friends with your very own hangman game! Send them your custom game and see if they have what it takes to solve it. Test their word-solving skills and find out who will emerge victorious!</p>
+                <div className='dialog-info-box'>
+                    <p>Challenge your friends with your very own hangman game! Send them your custom game and see if they have what it takes to solve it. Test their word-solving skills and find out who will emerge victorious!</p>
+                    <button className='button button-online' onClick={()=> handleClick("online")}>Play online</button>
                 </div>
             </div>
-            <div>
-                <p className='creator'>Created by <a href='https://sangyetenphel.com/'>Sangye Tenphel</a></p>
+            <div className='creator'>
+                <p>Created by <a href='https://sangyetenphel.com/'>Sangye Tenphel</a></p>
             </div>
         </div>
     )
