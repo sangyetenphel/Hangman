25a71284da92fd958a8437596c3e20af52dccb24
sangyetenphel <sangyetenphel@gmail.com>
2024-05-23 20:46:23 -0400

    Removed name and hint from backend

--- backend/hangman/views.py
+++ backend/hangman/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from api.models import Word
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
@@ -8,7 +8,7 @@ def hangman(request, id):
     word = get_object_or_404(Word, pk=id)
     word = {
         'name': word.name,
-        'word': word.word,
-        'hint': word.hint
+        # 'word': word.word,
+        # 'hint': word.hint
     }
     return JsonResponse(word)
